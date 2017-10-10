f1fcefbac15a09cc4a33e8de8791526728d227b5
ks4187 <ks4187@nyu.edu>
2017-10-09 21:25:59 -0400

    Fix api issues

--- src/pkjs/pebble-js-app.js
+++ src/pkjs/pebble-js-app.js
@@ -1,3 +1,50 @@
+var myAPIKey = 'a496197b748f1fe8fa154e0e6782ed43';
+
+function iconFromWeatherId(weatherId) {
+  if (weatherId < 600) {
+    return 2;
+  } else if (weatherId < 700) {
+    return 3;
+  } else if (weatherId > 800) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+/*function fetchWeather(latitude, longitude) {
+  var req = new XMLHttpRequest();
+  req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?' +
+    'lat=' + latitude + '&lon=' + longitude + '&cnt=1&appid=' + myAPIKey, true);
+  console.log('http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&cnt=1&appid=' + myAPIKey);
+  req.onload = function () {
+    //if (req.readyState === 4) {
+      if (req.status === 200) {
+        console.log(req.responseText);
+        var response = JSON.parse(req.responseText);
+        var temperature = Math.round(response.main.temp - 273.15);
+        var icon = iconFromWeatherId(response.weather[0].id);
+        var city = response.name;
+        console.log('temp: ' + temperature);
+        console.log('icon: ' + icon);
+        console.log('city: ' + city);
+        Pebble.sendAppMessage({
+          'WEATHER_ICON_KEY': icon,
+          'WEATHER_TEMPERATURE_KEY': temperature + '\xB0C',
+          'WEATHER_CITY_KEY': city
+        }, function() {
+          console.log('Message sent successfully');
+        }, function(e) {
+          console.log('MEssage failed: ' + e);
+        });
+      //} else {
+      //  console.log('Error');
+      //}
+    }
+  };
+  req.send(null);
+}*/
+
 function fetchWeather(latitude, longitude) {
   var req = new XMLHttpRequest();
   req.open('GET', 'https://ks4187.github.io/nylpay/api/payment_due.json', true);
@@ -6,6 +53,12 @@ function fetchWeather(latitude, longitude) {
       if (req.status === 200) {
         console.log(req.responseText);
         var response = JSON.parse(req.responseText);
+        /*var temperature = Math.round(response.main.temp - 273.15);
+        var icon = iconFromWeatherId(response.weather[0].id);
+        var city = response.name;
+        console.log('temp: ' + temperature);
+        console.log('icon: ' + icon);
+        console.log('city: ' + city);*/
         Pebble.sendAppMessage({
           'WEATHER_ICON_KEY': 4,
           'WEATHER_TEMPERATURE_KEY': response.payment_date,
@@ -31,8 +84,8 @@ function locationSuccess(pos) {
 function locationError(err) {
   console.warn('location error (' + err.code + '): ' + err.message);
   Pebble.sendAppMessage({
-    'WEATHER_CITY_KEY': 'Loc Unavailable',
-    'WEATHER_TEMPERATURE_KEY': 'N/A'
+    'WEATHER_CITY_KEY': 'PREMIUM DUE',
+    'WEATHER_TEMPERATURE_KEY': 'October 10th'
   });
 }
 
